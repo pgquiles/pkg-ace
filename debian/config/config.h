359410821282b5067473daa95775703fe3c07d78
Thomas Girard <thomas.g.girard@free.fr>
2005-11-23 22:04:26 +0000

    * Resurrect 5.4.2.1-1. Version 5.4.0.1-1.1 has *never* been released.

--- debian/config/config.h
+++ debian/config/config.h
@@ -7,27 +7,17 @@
 // Force exception support to be enabled.
 #define ACE_HAS_EXCEPTIONS 1
 
-// No need for the ACE_ASSERTions and RCSIDs since this is a "release"
-// (i.e. non-debug) build.
-#define ACE_NDEBUG 1
-#define ACE_USE_RCSID 0
-
 // Force support for timeprobes to be compiled.
 #define ACE_COMPILE_TIMEPROBES 1
 
 // Force support for POSIX Asynchronous IO (AIO) to be compiled.
 #define ACE_HAS_AIO_CALLS 1
 
-// BN: hmm, not sure if these are needed?
-#ifndef POSIX_THREADS
-#  define POSIX_THREADS
-#endif
-#ifndef _POSIX_THREAD_SAFE_FUNCTIONS
-#  define _POSIX_THREAD_SAFE_FUNCTIONS
-#endif
-#ifndef  _REENTRANT
-#  define _REENTRANT
-#endif
+// Force %S to work in ACE_DEBUG.
+#define ACE_HAS_SYS_SIGLIST 1
+
+// bossekr@debian.org: Get the system capabilities.
+#include <unistd.h>
 
 // -------------------------------
 
