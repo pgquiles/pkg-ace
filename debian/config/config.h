4f107c0ac9c493d71992256f5be28ed2a9ef696c
Raphael Bossek <raphael.bossek@gmx.de>
2005-11-30 21:40:09 +0000

    [einstein 2004-10-29 15:22:26 by br] * config/config.h, patches/20-debian-compiler-definitions.dpatch, patches/00list: Moved all compiler definitions to Makefile so they are set as soon as possible
    Original author: br
    Date: 2004-10-29 15:22:27

--- debian/config/config.h
+++ debian/config/config.h
@@ -1,21 +1,6 @@
 #ifndef ACE_CONFIG_H
 #define ACE_CONFIG_H
 
-// Force tools to use ACE_GPERF instead of 'gperf'
-#define ACE_GPERF "gperf-ace"
-
-// Force exception support to be enabled.
-#define ACE_HAS_EXCEPTIONS 1
-
-// Force support for timeprobes to be compiled.
-#define ACE_COMPILE_TIMEPROBES 1
-
-// Force support for POSIX Asynchronous IO (AIO) to be compiled.
-#define ACE_HAS_AIO_CALLS 1
-
-// Force %S to work in ACE_DEBUG.
-#define ACE_HAS_SYS_SIGLIST 1
-
 // bossekr@debian.org: Get the system capabilities.
 #include <unistd.h>
 
