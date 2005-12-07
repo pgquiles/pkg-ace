7594c96f4bd4b8e42c4679eb255ba3df3bbc9b5c
Thomas Girard <thomas.g.girard@free.fr>
2005-12-07 20:14:31 +0000

    Make sure reactor header files get deleted.
    Original author: Thomas Girard <thomas.g.girard@free.fr>
    Date: 2005-02-12 19:49:51

---
+++ debian/remove_reactor_dups.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+#
+# Remove reactor header files that should be distributed only with the 
+# lib{ace,tao}-{fl,qt,tk,xt}reactor-dev packages.
+#
+# Because there are wildcards in *.install files, we need to force
+# shell evaluation.
+
+DUPS=`cat debian/libace-*reactor-dev.install debian/libtao-*reactor-dev.install|grep include|cut -f 1 -d ' '|sed -e 's,^ace/,debian/libace-dev/usr/include/ace\/,g;s,^TAO/,debian/libtao-dev/usr/include/,g'`
+
+for fname_ in $DUPS; do
+    eval rm -f "$fname_"
+done
