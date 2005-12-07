b3be27368b451bee300f69682a457d57b69676d7
Thomas Girard <thomas.g.girard@free.fr>
2005-12-07 20:24:51 +0000

    Adapt remove_dups.sh to libtao-*resource change.
    Original author: Thomas Girard <thomas.g.girard@free.fr>
    Date: 2005-03-15 21:05:30

--- debian/remove_reactor_dups.sh
+++ debian/remove_reactor_dups.sh
@@ -6,7 +6,7 @@
 # Because there are wildcards in *.install files, we need to force
 # shell evaluation.
 
-DUPS=`cat debian/libace-*reactor-dev.install debian/libtao-*reactor-dev.install|grep include|cut -f 1 -d ' '|sed -e 's,^ace/,debian/libace-dev/usr/include/ace\/,g;s,^TAO/,debian/libtao-dev/usr/include/,g'`
+DUPS=`cat debian/libace-*reactor-dev.install debian/libtao-*resource-dev.install|grep include|cut -f 1 -d ' '|sed -e 's,^ace/,debian/libace-dev/usr/include/ace\/,g;s,^TAO/,debian/libtao-dev/usr/include/,g'`
 
 for fname_ in $DUPS; do
     eval rm -f "$fname_"
