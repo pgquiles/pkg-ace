c981b7de03b5763f89a9044a4ea7d26dfdd27672
Thomas Girard <thomas.g.girard@free.fr>
2006-07-09 09:36:54 +0000

    Don't make look-for-missing.sh too dependent on file command location.

--- debian/look-for-missing.sh
+++ debian/look-for-missing.sh
@@ -3,13 +3,13 @@
 LIBS_IN_INSTALL=`mktemp` || exit 1
 LIBS_IN_CONTROL=`mktemp` || exit 1
 
-CUT=/usr/bin/cut
-DIFF=/usr/bin/diff
-LS=/bin/ls
-GREP=/bin/grep
-RM=/bin/rm
-SED=/bin/sed
-SORT=/usr/bin/sort
+CUT=cut
+DIFF=diff
+LS=ls
+GREP=grep
+RM=rm
+SED=sed
+SORT=sort
 WD=`dirname $0`
 
 $LS $WD/*.install | $GREP "debian/lib" | $GREP -v -- -dev | $SED -e 's#.*debian/##' -e 's#\.install$##' | $SORT > $LIBS_IN_INSTALL
