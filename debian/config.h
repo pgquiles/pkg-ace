56d4609a41acecf45b0c93135171cfc124478745
Thomas Girard <thomas.g.girard@free.fr>
2010-05-02 14:12:48 +0000

    Reenable IPV6 and IPV6 IPV4 migration

--- debian/config.h
+++ debian/config.h
@@ -1,4 +1,6 @@
 #ifndef ACE_CONFIG_H_INCLUDED
 #define ACE_CONFIG_H_INCLUDED
+#define ACE_HAS_IPV6 1
+#define ACE_USES_IPV4_IPV6_MIGRATION 1
 #include "config-linux.h"
 #endif /* ACE_CONFIG_H_INCLUDED */
