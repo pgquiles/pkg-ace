dddba3d9968d413c7b387ebf0d22ba627d466e3b
Marek Brudka <mbrudka@aster.pl>
2010-09-11 09:41:42 +0000

    Updated generator_pkgconfig.sh and rules to reflect current install folders.

--- debian/generator_pkgconfig.sh
+++ debian/generator_pkgconfig.sh
@@ -13,38 +13,19 @@ fi
 #default values
 INCDIR="$\{prefix\}/include"
 
-IN=$(echo "$1" | sed -e "s#^$ACE_ROOT/##")
-
-case "$IN" in
- *ACE_SSL.pc.in)
-    INDIR="ace/SSL"
-    ;;
- *TAO_Utils.pc.in)
-    INDIR="tao/Utils"
-    ;;
- *orbsvcs/orbsvcs/TAO_*.pc.in)
-    INDIR="orbsvcs"
-    ;;
- *)
-    INDIR=`dirname $(echo $IN | sed -e 's#^\(TAO/\|ace/\|protocols/\)##')`
-    ;;
-esac
-
 case "$IN" in
  TAO/*)
-    VERSION="$TV"
+    VERSION="$PKGCONFIG_TV"
     ;;
  *)
-    VERSION="$AV"
+    VERSION="$PKGCONFIG_AV"
     ;;
 esac
 
-if test "s$INDIR" != "s." ; then
-    INCDIR="$INCDIR/$INDIR"
-fi
 
 cat "$1" | sed -e 's#@prefix@#/usr#g' \
                -e 's#@exec_prefix@#$\{prefix\}#g' \
                -e 's#@libdir@#$\{exec_prefix\}/lib#g' \
                -e "s#@includedir@#$INCDIR#g" \
-               -e "s#@VERSION@#$VERSION#g"
+               -e "s#@VERSION@#$VERSION#g" \
+               -e "s#@LIBS@##g"
